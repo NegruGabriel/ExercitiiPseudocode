9ff35de88a1a38193f52b7f7770968f0b720695c
NegruGabriel <87751537+NegruGabriel@users.noreply.github.com>
2022-05-04 13:41:00 +0200

    Fixat problema 3

--- src/Problem3/Problem3.java
+++ src/Problem3/Problem3.java
@@ -4,39 +4,36 @@ import java.util.Scanner;
 
 public class Problem3 {
     public static void main(String[] args) {
-
-        //Write a program that prints the next 20 leap years.
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Read year: ");
-        int yourYear = sc.nextInt();
-        // System.out.println("The  year is: " + yourYear);
-
-        int c = 20;
-        int count = 0;
-        while (count != c) {
-            yourYear = yourYear + 1;
-            if ((yourYear % 400 == 0 || yourYear % 4 == 0)) {
-                System.out.println(yourYear);
-                count++;
-            } else if ((yourYear % 100 == 0)) {
-                System.out.println("no year");
-
+        //Write a program that prints the  20 leap years.
+        int year = 2022;
+        int counter = 0;
+        while (counter != 20) {
+            if (year % 400 == 0) {
+                System.out.println(year);
+                counter++;
+            } else if (year % 100 == 0) {
+            } else if (year % 4 == 0) {
+                System.out.println(year);
+                counter++;
             }
+            year++;
         }
+
     }
 }
+
             //Pseodocode
-            //citeste an;
+            //Read Year;
             //counter = 0;
-            //cat timp counter != 20
-            //daca an % 400 == 0 atunci
-            //afiseaza an
+            //while counter != 20
+            //If Year % 400 == 0 atunci
+            //Show Year
             //counter ++
-            //daca an % 100 == 0 atunci
-            //nu afisa an
-            //daca an % 4 == 0 atunci
-            //afiseaza an
+            //If Year % 100 == 0 Else
+            //don't show Year
+            //If Year % 4 == 0 Else
+            //Show Year
             //counter++
-            //an++
+            //Year++
 
 
