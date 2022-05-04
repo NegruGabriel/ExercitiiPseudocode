c594e523bcaa6b8becd428db6145f3575e00b1cc
NegruGabriel <87751537+NegruGabriel@users.noreply.github.com>
2022-05-04 16:55:12 +0200

    problema 4 rezolvare

--- src/Problem4/Problem4.java
+++ src/Problem4/Problem4.java
@@ -4,30 +4,32 @@ import java.util.Scanner;
 
 public class Problem4 {
     public static void main(String[] args) {
+        //Write a program that solves a quadric equation
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a: ");
         double a = input.nextDouble();
+
         System.out.println("Enter b: ");
         double b = input.nextDouble();
+
         System.out.println("Enter c: ");
         double c = input.nextDouble();
 
+        double x1 = input.nextDouble();
+        double x2 = input.nextDouble();
+
+        x1 =-b + Math.sqrt(b*b-4*a*c)/2*a;
+        x2 =-b - Math.sqrt(b*b-4*a*c)/2*a;
+
+        System.out.println(x1);
+        System.out.println(x2);
     }
-
-
-
-
-
-
-
-
-
-    //Pseodocode
-    //Read  a;
-    //Read  b;
-    //Read  c;
-    //solutionOne = -b + sqrt(b^2 - 4 * a * c) / 2 * a
-    //solutionTwo = -b - sqrt(b^2 - 4 * a * c) / 2 * a
-    //afiseaza solutionTwo;
-    //afiseaza solutionOne;
 }
+//Pseodocode
+//Read  a;
+//Read  b;
+//Read  c;
+//solutionOne = -b + sqrt(b^2 - 4 * a * c) / 2 * a
+//solutionTwo = -b - sqrt(b^2 - 4 * a * c) / 2 * a
+//Show solutionOne;
+//Show solutionTwo;
