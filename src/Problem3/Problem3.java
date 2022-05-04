ce6c776052ddfc966f16ef94cb1d193338412dc9
NegruGabriel <87751537+NegruGabriel@users.noreply.github.com>
2022-05-04 11:31:19 +0200

    De Rezolvat poroblema 3

--- src/Problem3/Problem3.java
+++ src/Problem3/Problem3.java
@@ -9,29 +9,39 @@ public class Problem3 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Read year: ");
         int yourYear = sc.nextInt();
-        System.out.println("The  year is: " + yourYear);
+        // System.out.println("The  year is: " + yourYear);
 
         int c = 20;
         int count = 0;
-        while (count !=c){
-            yourYear = yourYear +1;
-            if((yourYear%400 == 0)|| (yourYear% 100 !=0)){
-                count++;
+        while (count != c) {
+            yourYear = yourYear + 1;
+            if ((yourYear % 400 == 0)) {
                 System.out.println(yourYear);
+                count++;
+                else if ((yourYear % 100 == 0)) {
+                    System.out.println("no year");
+
+                }
+                if ((yourYear % 4 == 0)) {
+                    System.out.println(yourYear);
+                    count++;
+                    yourYear++;
+
+                }
             }
+            //Pseodocode
+            //citeste an;
+            //counter = 0;
+            //cat timp counter != 20
+            //daca an % 400 == 0 atunci
+            //afiseaza an
+            //counter ++
+            //daca an % 100 == 0 atunci
+            //nu afisa an
+            //daca an % 4 == 0 atunci
+            //afiseaza an
+            //counter++
+            //an++
         }
-        //Pseodocode
-        //citeste an;
-        //counter = 0;
-        //cat timp counter != 20
-        //daca an % 400 == 0 atunci
-        //afiseaza an
-        //counter ++
-        //daca an % 100 == 0 atunci
-        //nu afisa an
-        //daca an % 4 == 0 atunci
-        //afiseaza an
-        //counter++
-        //an++
     }
 }
